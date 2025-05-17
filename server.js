8194c5a82a67abb63bac543131baf980c0da2e15
renan <renan.menegheti@hotmail.com>
2025-05-17 12:50:47 -0300

    Get product by id merge

--- server.js
+++ server.js
@@ -74,6 +74,19 @@ fastify.get('/produtos', {
     }
 });
 
+// Fastify route to get specific product by id
+fastify.get('/produtos/:id', async (request, reply) => {
+    const id = request.params.id;
+    try {
+        const data = await db.all('SELECT id, name, description, price, category, pictureUrl FROM produtos WHERE id = ?', [id]);
+        const produto = new Produto(data[0].id, data[0].name, data[0].description, data[0].price, data[0].category, data[0].pictureUrl);
+        reply.code(200).send(produto);
+    } catch (error) {
+        console.error(error);
+        reply.code(500).send({ error: 'Erro ao deletar produto' })
+    }
+});
+
 fastify.post('/produtos', {
     ...postProdutoOpts,
     onRequest: (request, reply, done) => {
