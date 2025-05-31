ae2edc457a529aaa87718c7cdeb09bed230b4678
anacsza <ana.caroline.souza09@gmail.com>
2025-05-31 13:22:39 -0300

    Adicionado seed config e inicializacao da tabela produtos a partir do json processed

---
+++ seeds/products_init.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+exports.seed = async function (knex) {
+  await knex.transaction(async (trx) => {
+    try {
+      console.log('Limpando tabela produtos...');
+      await trx('produtos').del();
+      console.log('Tabela produtos limpa com sucesso.');
+      console.log('Inserindo dados iniciais na tabela produtos...');
+      const filePath = path.join(__dirname, 'processed.json');
+      const data = await fsPromises.readFile(filePath, 'utf-8');
+      const produtos = JSON.parse(data);
+      await trx('produtos').insert(produtos.map(produto => ({
+        id: produto.id,
+        name: produto.name,
+        description: '',
+        price: 0,
+        category: produto.category,
+        pictureUrl: ''
+      })));
+      await trx.commit();
+      console.log('Produtos iniciais carregados com sucesso.');
+    } catch (error) {
+      console.error('Erro ao inserir dados iniciais:', error.message);
+      await trx.rollback();
+      throw err;
+    }
+  });
+  console.log('Seed de produtos concluído.');
+};
